3eefa33242f0abf4b9b113c26afa08fb3f410e5f
ailshelvira <ailshelvira@gmail.com>
2020-11-05 20:03:13 +0000

    some fixes

--- 1create_classes.rb
+++ 1create_classes.rb
@@ -1,5 +1,5 @@
 class Station
-  attr_reader :name
+  attr_reader :name, :trains
 
   def initialize(name)
     @name = name
@@ -7,24 +7,15 @@ class Station
   end
 
   def take_train(train)
-    @trains << train unless @trains.include?(train)
+    trains << train unless @trains.include?(train)
   end
 
   def dispatch_train(train)
-    @trains.delete(train)
-  end
-
-  def show_trains
-    @trains.each { |train| puts train }
+    trains.delete(train)
   end
 
   def show_type_sorted_trains
-    passenger_trains = @trains.select {|train| train.type == 'passenger'}
-    freight_trains = @trains.select {|train| train.type == 'freight'}
-    puts "Количество пассажирских: #{passenger_trains.length}"
-    passenger_trains.each { |train| puts train }
-    puts "Количество грузовых: #{freight_trains.length}"
-    freight_trains.each { |train| puts train }
+    trains.sort_by(&:type)
   end
 end
 
@@ -36,22 +27,17 @@ class Route
   end
 
   def add_station(station)
-    unless stations.include?(station)
-      finish_index = stations.length - 1
+    return if stations.include?(station)
 
-      finish_station = stations.pop
-      stations[finish_index] = station
-      stations.push(finish_station)
-    end
+    finish_index = stations.length - 1
+
+    finish_station = stations.pop
+    stations[finish_index] = station
+    stations.push(finish_station)
   end
 
   def remove_station(station)
     stations.delete(station) if stations.index(station) != 0 && stations.index(station) != stations.length - 1
-  end
-
-  # что выводить требуется наименования или станции как объекты
-  def show_stations
-    stations.each { |station| puts station }
   end
 end
 
@@ -80,56 +66,45 @@ class Train
 
   def route=(new_route)
     if route
-      cur_station_index = self.route.stations.index(current_station)
+      cur_station_index = route.stations.index(current_station)
       route.stations[cur_station_index].dispatch_train(self)
     end
 
     @route = new_route
 
-    @current_station = self.route.stations[0]
-    self.route.stations[0].take_train(self)
+    @current_station = route.stations[0]
+    route.stations[0].take_train(self)
   end
 
   def prev_station
     station_index = route.stations.index(current_station)
-    if route && !station_index.zero?
-      route.stations[station_index - 1]
-    end
+
+    return if !route && station_index.zero?
+
+    route.stations[station_index - 1]
   end
 
   def next_station
     station_index = route.stations.index(current_station)
-    if route && station_index != route.stations.length - 1
-      route.stations[station_index + 1]
-    end
+
+    return if !route && station_index == route.stations.length - 1
+
+    route.stations[station_index + 1]
   end
 
   def move_forward
-    if route
-      cur_station_index = route.stations.index(current_station)
-      if cur_station_index != route.stations.length - 1
-        next_station_index = route.stations.index(next_station)
+    return unless next_station
 
-        route.stations[cur_station_index].dispatch_train(self)
-        @current_station = next_station
-        route.stations[next_station_index].take_train(self)
-      end
-    end
+    current_station.dispatch_train(self)
+    @current_station = next_station
+    current_station.take_train(self)
   end
 
   def move_back
-    if route
-      cur_station_index = route.stations.index(current_station)
+    return unless prev_station
 
-      if cur_station_index != 0
-        puts("nihao")
-        prev_station_index = route.stations.index(prev_station)
-
-        route.stations[cur_station_index].dispatch_train(self)
-        @current_station = prev_station
-        route.stations[prev_station_index].take_train(self)
-      end
-    end
+    current_station.dispatch_train(self)
+    @current_station = prev_station
+    current_station.take_train(self)
   end
-
 end
