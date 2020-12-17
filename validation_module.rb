8cefbe389c114319e0a29e34b850a42b120c624a
ailshelvira <ailshelvira@gmail.com>
2020-12-18 01:07:36 +0300

    clean up methods

--- validation_module.rb
+++ validation_module.rb
@@ -11,48 +11,34 @@ module Validation
 
     def validate(attr_name, validation_type, optional_arg = nil)
       var_name = "@#{attr_name}".to_sym
-
-      case validation_type
-
-      when :presence
-        validators.push({ type: :presence, var: var_name })
-
-      when :format
-        validators.push({ type: :format, var: var_name, args: optional_arg })
-
-      when :type
-        validators.push({ type: :type, var: var_name, args: optional_arg })
-
-      else raise 'Wrong argument is received'
-      end
+      validators.push(type: validation_type, var: var_name, args: optional_arg)
     end
   end
 
   module InstanceMethods
-    def presence_validation(var)
-      value = instance_variable_get(var)
+    def presence_validation(value)
       raise "Attribute value shouldn't be nil or empty string" if value.nil? || value.empty? || value.strip.empty?
     end
 
-    def type_validation(var, class_name)
-      value = instance_variable_get(var)
-      raise "Attribute value doesn't belong to #{class_name} class" if value.class != class_name 
+    def type_validation(value, class_name)
+      raise "Attribute value doesn't belong to #{class_name} class" if value.class != class_name
     end
 
-    def format_validation(var, regexp)
-      value = instance_variable_get(var)
+    def format_validation(value, regexp)
       raise "Attribute value doesn't match the regexp" if value.match(regexp).nil?
     end
 
     def validate!
       self.class.validators.each do |validator|
+        value = instance_variable_get(validator[:var])
+
         case validator[:type]
         when :presence
-          presence_validation validator[:var]
+          presence_validation value
         when :type
-          type_validation validator[:var], validator[:args]
+          type_validation value, validator[:args]
         when :format
-          format_validation validator[:var], validator[:args]
+          format_validation value, validator[:args]
         end
       end
     end
